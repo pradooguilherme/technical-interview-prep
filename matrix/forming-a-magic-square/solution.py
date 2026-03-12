74e68961d65976a946ed7983554e19d8344fa000
Guilherme Prado <143662365+pradooguilherme@users.noreply.github.com>
2026-03-12 21:19:39 +0000

    forming a magic square solution

---
+++ matrix/forming-a-magic-square/solution.py
@@ -0,0 +1,60 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'formingMagicSquare' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY s as parameter.
+#
+
+def compare_two_matrix(s, t) -> int:
+    cost = 0
+    for i in range(len(s)):
+        for j in range(len(s[0])):
+            cost += abs(s[i][j] - t[i][j])
+    return cost
+
+def formingMagicSquare(s):
+    valid_squares = [
+    [[8, 1, 6], [3, 5, 7], [4, 9, 2]],
+    [[6, 1, 8], [7, 5, 3], [2, 9, 4]],
+    [[4, 9, 2], [3, 5, 7], [8, 1, 6]],
+    [[2, 9, 4], [7, 5, 3], [6, 1, 8]],
+    [[8, 3, 4], [1, 5, 9], [6, 7, 2]],
+    [[4, 3, 8], [9, 5, 1], [2, 7, 6]],
+    [[6, 7, 2], [1, 5, 9], [8, 3, 4]],
+    [[2, 7, 6], [9, 5, 1], [4, 3, 8]]
+    ]
+    
+    lower_cost = float('inf')
+    
+    for magic_square in valid_squares:
+        cost = compare_two_matrix(s, magic_square)
+        
+        if cost < lower_cost:
+            lower_cost = cost
+            
+    return lower_cost
+    
+    
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = []
+
+    for _ in range(3):
+        s.append(list(map(int, input().rstrip().split())))
+
+    result = formingMagicSquare(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
